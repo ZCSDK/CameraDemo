60c4097b6ed6c1ba36b9a3a5f2bb54af0fb2ede4
hero-wangshuai <122675799@qq.com>
2017-12-14 16:44:55 +0800

    去掉关于voice的

--- README.md
+++ README.md
@@ -3,10 +3,3 @@
 一个 按钮调用相册文件
 一个按钮 调用照相机
 并且 把图片显示到ImageView上
-
-
-# 在demo module 中   VoiceActivity是关于语音的功能
-
-sendType == 0  正在录音
-sendType == 1  发送录音
-sendType == 2  取消录音
--- voice_tools/src/main/java/com/sobot/voice/MySendVoice.java
+++
@@ -1,9 +0,0 @@
-package com.sobot.voice;
-
-/**
- * Created by Administrator on 2017/11/29.
- */
-
-public interface MySendVoice{
-    void sendVoice(int sendType, String voiceMsgId,String mFileName, int voiceLongTime);
-}
